51865bd37c51b962f4b69bf5fae14c70f8bdf629
LayxProud <49621292+LayxProud@users.noreply.github.com>
2020-12-26 20:25:24 +0300

    Add files via upload

--- lab2a/main.c
+++ lab2a/main.c
@@ -4,11 +4,13 @@
 #include <locale.h>
 #include <time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 	int pipedes[2];
+	
 	if (pipe(pipedes) == -1) 
 	{
         perror("pipe");
@@ -19,39 +21,45 @@ int main()
 	pipe(pipedes);
 	switch(pid = fork())
 	{
-	
-		case -1: // произошла ошибка
-		perror("fork");
-		exit(1);
 		
-		case 0:	// дочерний процесс	
-		sleep(5); // устанавливает задержку 5 секунд
-		printf("\n CHILD: Получаю системное время из pipe\n");
-		close(pipedes[1]); // дочерний процесс закрывает выходную часть pipe
+		case -1: 				// произошла ошибка
+			perror("fork");
+			exit(1);
+		
+		case 0:					// дочерний процесс	
+			printf("\n CHILD: Получаю системное время из pipe\n");
+			close(pipedes[1]); 	// дочерний процесс закрывает выходную часть pipe
+			sleep(5); 			// устанавливает задержку 5 секунд
+			
+			long int child_buf[2];
+			time_t child_time = time(NULL);
+			printf("\n CHILD: Время дочернего процесса -- %s", ctime(&child_time));
+		
+			read(pipedes[0], &child_buf[0], sizeof child_buf);
+			printf(" CHILD: Время родительского процесса -- %s", ctime(&child_buf[0]));
+		
+			read(pipedes[0], &child_buf[1], sizeof child_buf);
+			printf(" CHILD: Родительский PID: %ld\n", child_buf[1]);
 
-		long int child_buf[2];
-		time_t child_time = time(NULL);
-		printf("\n CHILD: Время дочернего процесса -- %s", ctime(&child_time));
+			close(pipedes[0]);
+			exit(0);
 		
-		read(pipedes[0], &child_buf[0], sizeof child_buf);
-		printf(" CHILD: Время родительского процесса -- %s", ctime(&child_buf[0]));
+		default: 				// родительский процесс
+			printf("\n PARENT: Помещаю системное время в pipe\n");
+			close(pipedes[0]); 	// родительский процесс закрывает входную часть pipe
 		
-		read(pipedes[0], &child_buf[1], sizeof child_buf);
-		printf(" CHILD: Родительский PID: %ld\n", child_buf[1]);
-
-		close(pipedes[0]);
-		exit(0);
+			long int parent_buf[2];
+			parent_buf[0] = time(NULL);
+			parent_buf[1] = getpid();
 		
-		default: // родительский процесс
-		printf("\n PARENT: Помещаю системное время в pipe\n");
-		close(pipedes[0]); // родительский процесс закрывает входную часть pipe
-		
-		long int parent_buf[2];
-		parent_buf[0] = time(NULL);
-		parent_buf[1] = getpid();
-		
-		write(pipedes[1], parent_buf, sizeof parent_buf);
-		close(pipedes[1]);	
+			write(pipedes[1], parent_buf, sizeof parent_buf);
+			close(pipedes[1]);	
+			
+			if (waitpid(pid, 0, 0) == -1) 
+			{
+				perror("waitpid");
+				exit(1);
+			}
 	}
 	
 	return 0;
