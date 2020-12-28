c84d38c54876a6f9053b6bc301ccb17f7ae85de7
LayxProud <49621292+LayxProud@users.noreply.github.com>
2020-12-29 02:29:56 +0300

    Add files via upload

--- lab2b/main.c
+++ lab2b/main.c
@@ -13,8 +13,12 @@
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	
+	char buf[80];
+	const int bufsize = 80;
 	int fifodes;
 	pid_t pid;
+	
 	unlink (PATH);
 	mkfifo(PATH, 0777);
 	
@@ -26,8 +30,8 @@ int main()
 		exit(1);
 	
 		case 0: // дочерний процесс
-		printf("\n CHILD: Получаю системное время из fifo\n");
 		fifodes = open(PATH, O_RDONLY);
+		
 		if(fifodes)
 		{
 			sleep(5);
@@ -35,26 +39,27 @@ int main()
 			long int child_buf[2];
 			time_t child_time = time(NULL);
 			
-			read(fifodes, child_buf, sizeof child_buf);
+			read(fifodes, buf, bufsize);
 			printf("\n CHILD: Время дочернего процесса -- %s", ctime(&child_time));
-			printf(" CHILD: Время родительского процесса -- %s", ctime(&child_buf[0]));
-			printf(" CHILD: Родительский PID: %ld\n", child_buf[1]);
+			printf(" CHILD: Получил от родителя следующие данные:\n%s", buf);
 
 			close(fifodes);
 			exit(0);
 		}
 		
 		default: // родительский процесс
-		printf("\n PARENT: Помещаю системное время в fifo\n");
 		fifodes = open(PATH, O_WRONLY);
 		
 		if(fifodes)
 		{
 			long int parent_buf[2];
+			time_t parent_time = time(NULL);
 			parent_buf[0] = time(NULL);
 			parent_buf[1] = getpid();
+			
+			sprintf(buf, " PARENT: Мой PID -- %d\n Моё время -- %s\n", getpid(), ctime(&parent_time));
 
-			write(fifodes, parent_buf, sizeof parent_buf);
+			write(fifodes, buf, bufsize);
 			
 			close(fifodes);
 		}
